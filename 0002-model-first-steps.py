328df0a6fc00031ce7e9d041549f123952c950b1
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-08-20 22:15:44 +0300

    Create 0002-model-first-steps.py

---
+++ 0002-model-first-steps.py
@@ -0,0 +1,16 @@
+target_sentence = "How is it going on?" # Target sentence
+
+characters_usable = "abcdefghijklmnopqrstuvwxyz?"
+
+# let's define the learning rate
+
+alphe_learning_rate = 0.1
+
+# let's define the discount factor for the future rewards
+
+gamma_discount_rate = 0.9
+
+# number of epochs
+
+epochs = 5000
+
