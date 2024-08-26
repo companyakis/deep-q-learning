8879f933884db874e276ec25088d1cad91e80c98
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-08-26 21:37:50 +0300

    Delete 0009-taxi-002.py

--- 0009-taxi-002.py
+++
@@ -1,9 +0,0 @@
-import gymnasium as gym
-
-# create our environment
-
-env = gym.make("Taxi-v3", render_mode = "ansi") # text-based mode suitable for displaying the environment in a text console
-
-env.reset()
-
-print(env.render())
